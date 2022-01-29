871e3917e8f4fd64392dd1e509a0da045e49615e
superlaser97 <lowzhiheng@gmail.com>
2022-01-30 03:21:26 +0800

    1

--- scripts.js
+++ scripts.js
@@ -1,10 +1,5 @@
-// Create a struct for the data
-// struct contains the following:
-// 		- name
-// 		- description
-// 		- image
-// 		- price
-// 		- quantity
+var lineBreak = "<br>";
+
 class Person 
 {
     constructor(name, description, image, price, quantity)
@@ -33,30 +28,54 @@ function jsonToArray(json)
 // Function that takes in inputs from multiple textbox and returns a person object
 function UpdateText()
 {
-    var name = document.getElementsByTagName("name").value;
-    var description = document.getElementsByTagName("description").value;
-    var image = document.getElementsByTagName("image").value;
-    var price = document.getElementsByTagName("price").value;
-    var quantity = document.getElementsByTagName("quantity").value;
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+    var image = document.getElementById("image").value;
+    var price = document.getElementById("price").value;
+    var quantity = document.getElementById("quantity").value;
     var person = new Person(name, description, image, price, quantity);
-
-    // display the person's name in a prompt
-    alert(person.name);
 
     // Convert the person object to a string using formatPerson function
     // and update the label using updateLabel function
-    updateLabel(document.getElementById("inputtedPerson"), formatPerson(person));
+    //updateLabel(document.getElementById("inputtedPerson"), formatPerson(person));
+    updateLabel(document.getElementById("inputtedPerson"), serializePerson(person));
+    addPerson(person);
+}
+
+// Function that adds a person object to the table
+function addPerson(person)
+{
+    var table = document.getElementById("table");
+    var row = table.insertRow(table.rows.length);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+
+    cell1.innerHTML = person.name;
+    cell2.innerHTML = person.description;
+    cell3.innerHTML = person.image;
+    cell4.innerHTML = person.price;
+    cell5.innerHTML = person.quantity;
+}
+
+// Function that serializes a person object to a json string
+function serializePerson(person)
+{
+    var json = JSON.stringify(person);
+    return json;
 }
 
 // Function that takes in a person object and format it as a string
 function formatPerson(person)
 {
     var str = "";
-    str += "Name: " + person.name + "\n";
-    str += "Description: " + person.description + "\n";
-    str += "Image: " + person.image + "\n";
-    str += "Price: " + person.price + "\n";
-    str += "Quantity: " + person.quantity + "\n";
+    str += "Name: " + person.name + lineBreak;
+    str += "Description: " + person.description + lineBreak;
+    str += "Image: " + person.image + lineBreak;
+    str += "Price: " + person.price + lineBreak;
+    str += "Quantity: " + person.quantity + lineBreak;
     return str;
 }
 
