f57aac29296b8a58931a2272aa2454029e9c1773
superlaser97 <lowzhiheng@gmail.com>
2022-05-30 20:49:39 +0800

    Update script.ts

--- script.ts
+++ script.ts
@@ -364,12 +364,14 @@ function UpdateUnrosteredPlayers(): void
 
     unrosteredPlayersTable.innerHTML = "";
     let unrosteredPlayers: string[] = [];
-    let unrosteredPlayersInSessionNum: string[] = [];
 
     for(let sessionIndex = 0; sessionIndex < 10; sessionIndex++)
     {
         let unrosteredPlayersInSession: string[] = GetUnrosteredPlayersFromSession(sessionIndex);
         let unrosteredPlayersInSession_string: string = "";
+
+        unrosteredPlayersInSession_string += unrosteredPlayersInSession.length + " Players unrostered";
+        unrosteredPlayersInSession_string += "--------" + "<br>";
 
         // foreach unrosteredPlayersInSession
         for(let i = 0; i < unrosteredPlayersInSession.length; i++)
@@ -377,11 +379,9 @@ function UpdateUnrosteredPlayers(): void
             unrosteredPlayersInSession_string += unrosteredPlayersInSession[i] + "<br>";
         }
         unrosteredPlayers.push(unrosteredPlayersInSession_string);
-        unrosteredPlayersInSessionNum.push(unrosteredPlayersInSession.length + " Players");
     }
         
     AddRowToTable("unrosteredPlayersTable", unrosteredPlayers);
-    AddRowToTable("unrosteredPlayersTable", unrosteredPlayersInSessionNum);
 }
 
 
